f5b3fc16ad34aa5b0e6a902d051bd9b3e5d055d6
Poonam Kushwaha <uramazing247@gmail.com>
2025-06-08 12:31:56 +0530

    Updated code

--- import.tf
+++ import.tf
@@ -1,4 +1,4 @@
-import {
-  id = "/subscriptions/c3f8050d-d673-458d-bec0-8ff5c1c84f79/resourceGroups/RG-TH-01"
-  to = azurerm_resource_group.example
-}
+# import {
+#   id = "/subscriptions/c3f8050d-d673-458d-bec0-8ff5c1c84f79/resourceGroups/RG-TH-01"
+#   to = azurerm_resource_group.example
+# }
--- main.tf
+++ main.tf
@@ -4,7 +4,20 @@ resource "azurerm_resource_group" "example" {
 }
 
 
-# resource "azurerm_resource_group" "example2" {
-#   name     = "RG-TH-02"
-#   location = "West Europe"
-# }
+resource "azurerm_resource_group" "example2" {
+  name     = "RG-TH-02"
+  location = "West Europe"
+}
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = "sto890ountn345"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
